ebef981f175020a3899e87d8027ee5feebb2a13a
AlexCarterG <artemev.gregory1777@gmail.com>
2024-11-26 07:59:45 +0300

    Add controllers for backend operations

---
+++ src/backend/controllers/cluster_controller.py
@@ -0,0 +1,15 @@
+from ..models.cluster_model import ClusterModel
+
+class ClusterController:
+    """Controller for cluster operations."""
+    def get_all_clusters(self):
+        return ClusterModel.get_all()
+    
+    def get_cluster(self, cluster_id):
+        return ClusterModel.get(cluster_id)
+    
+    def create_cluster(self, cluster_data):
+        return ClusterModel.create(cluster_data)
+    
+    def update_cluster(self, cluster_id, cluster_data):
+        return ClusterModel.update(cluster_id, cluster_data)
---
+++ src/backend/controllers/wallet_controller.py
@@ -0,0 +1,15 @@
+from ..models.wallet_model import WalletModel
+
+class WalletController:
+    """Controller for wallet operations."""
+    def get_all_wallets(self):
+        return WalletModel.get_all()
+    
+    def get_wallet(self, wallet_id):
+        return WalletModel.get(wallet_id)
+    
+    def create_wallet(self, wallet_data):
+        return WalletModel.create(wallet_data)
+    
+    def update_wallet(self, wallet_id, wallet_data):
+        return WalletModel.update(wallet_id, wallet_data)
