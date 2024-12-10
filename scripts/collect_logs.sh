29d2723c73663062d7f17559c881b34f0a3f3e99
mllover22 <shlukha.dyryavya@mail.ru>
2024-12-10 13:42:03 +0300

    Add timestamp to collect_logs archive filename

--- scripts/collect_logs.sh
+++ scripts/collect_logs.sh
@@ -3,3 +3,6 @@ LOG_DIR="logs"
 ARCHIVE="logs_$(date +%Y%m%d%H%M%S).tar.gz"
 tar -czf $ARCHIVE $LOG_DIR
 echo "Logs archived to $ARCHIVE"
+                                
+                                
+                                
