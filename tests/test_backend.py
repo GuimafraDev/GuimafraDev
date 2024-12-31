ba36d3b7d6659ad8a98233ea7187600969e4379b
jameshudson12 <polina.bebra@mail.ru>
2024-12-31 17:28:40 +0300

    Update backend tests with new routes

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -17,3 +17,8 @@ class TestBackend(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+                                                       
+                                                       
+                                                       
+                                                       
+                                                       
