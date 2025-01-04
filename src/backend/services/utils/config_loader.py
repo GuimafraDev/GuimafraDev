8f0934a0be4ee9ed1d1fa0e319165fa7fe490728
jameshudson12 <polina.bebra@mail.ru>
2025-01-05 02:40:47 +0300

    Improve config loader to support multiple formats

--- src/backend/services/utils/config_loader.py
+++ src/backend/services/utils/config_loader.py
@@ -12,5 +12,9 @@ class ConfigLoader:
                                               
                                               
                                               
-                                              
-                                              
+                   
+                   
+                   
+                   
+                   
+                   
