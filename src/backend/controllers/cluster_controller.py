166499b34ae43032bbcb943e1dc8e484b8e5776a
AlexCarterG <artemev.gregory1777@gmail.com>
2025-01-04 13:04:27 +0300

    Enhance cluster_controller with input validation

--- src/backend/controllers/cluster_controller.py
+++ src/backend/controllers/cluster_controller.py
@@ -19,5 +19,9 @@ class ClusterController:
                                            
                                            
                                            
-                                           
-                                           
+    
+    
+    
+    
+    
+    
