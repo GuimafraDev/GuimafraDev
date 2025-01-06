f31077efa6a23f36b708adb13987ead0fa6c488c
AlexCarterG <artemev.gregory1777@gmail.com>
2025-01-06 04:26:23 +0300

    Improve tool_connector error reporting

--- src/ai_agent/tool_connector.py
+++ src/ai_agent/tool_connector.py
@@ -17,5 +17,10 @@ class ToolConnector:
     
     
     
-    
-    
+                                
+                                
+                                
+                                
+                                
+                                
+                                
