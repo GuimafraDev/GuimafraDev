dae6ab1d44c62093d0cb90802d4d4dfb324baa32
AlexCarterG <artemev.gregory1777@gmail.com>
2025-01-06 03:08:16 +0300

    Update prompt_manager for dynamic prompt adjustments

--- src/ai_agent/prompt_manager.py
+++ src/ai_agent/prompt_manager.py
@@ -25,6 +25,11 @@ class PromptManager:
                                
                                
                                
-                               
-                               
-                               
+                                                                                            
+                                                                                            
+                                                                                            
+                                                                                            
+                                                                                            
+                                                                                            
+                                                                                            
+                                                                                            
