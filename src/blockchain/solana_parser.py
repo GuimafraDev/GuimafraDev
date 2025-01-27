8c07be7c917400a90b3546e93599586472a9cd6e
jameshudson12 <polina.bebra@mail.ru>
2025-01-27 08:35:38 +0300

    Improve solana_parser to handle nested JSON

--- src/blockchain/solana_parser.py
+++ src/blockchain/solana_parser.py
@@ -26,5 +26,9 @@ class SolanaParser:
                                          
                                          
                                          
-                                         
-                                         
+                                                                                     
+                                                                                     
+                                                                                     
+                                                                                     
+                                                                                     
+                                                                                     
