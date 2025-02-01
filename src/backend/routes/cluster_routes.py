b9b14a21fa4c169abb2fee36a235eb0597621646
mllover22 <shlukha.dyryavya@mail.ru>
2025-02-01 16:57:57 +0300

    Enhance cluster_routes with advanced sorting

--- src/backend/routes/cluster_routes.py
+++ src/backend/routes/cluster_routes.py
@@ -38,6 +38,10 @@ def update_cluster(cluster_id):
                             
                             
                             
-                            
-                            
-                            
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
