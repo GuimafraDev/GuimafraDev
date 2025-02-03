99ef30fbfcb8b233e5ee3600c212a96399068a0b
AlexCarterG <artemev.gregory1777@gmail.com>
2025-02-03 06:32:37 +0300

    Refactor wallet_model for transaction aggregation

--- src/backend/models/wallet_model.py
+++ src/backend/models/wallet_model.py
@@ -42,6 +42,12 @@ class WalletModel:
                                  
                                  
                                  
-                                 
-                                 
-                                 
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
